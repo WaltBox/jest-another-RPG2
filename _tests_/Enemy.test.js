3d77627fabd9a94956c0a7c6b79ecfe6773ecfca
Walt Boxwell <waltboxwell@gmail.com>
2022-05-12 12:35:56 -0500

    testing problem pt 2

--- _tests_/Enemy.test.js
+++ _tests_/Enemy.test.js
@@ -1,17 +1,15 @@
 const Enemy = require('../lib/Enemy.js');
-
 const Potion = require('../lib/Potion.js');
-
 
 jest.mock('../lib/Potion.js');
 
-test('creates an Enemy object', () => {
-    const enemy = new Enemy('goblin', 'sword');
+test('creates an enemy object', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-    expect(Enemy.name).toBe('goblin');
-    expect(Enemy.weapon).toBe(sword);
-    expect(Enemy.health).toEqual(expect.any(Number));
-    expect(Enemy.strength).toEqual(expect.any(Number));
-    expect(Enemy.agility).toEqual(expect.any(Number));
-    expect(Enemy.potion).toEqual(expect.any(Object));
+  expect(enemy.name).toBe('goblin');
+  expect(enemy.weapon).toBe('sword');
+  expect(enemy.health).toEqual(expect.any(Number));
+  expect(enemy.strength).toEqual(expect.any(Number));
+  expect(enemy.agility).toEqual(expect.any(Number));
+  expect(enemy.potion).toEqual(expect.any(Object));
 });
